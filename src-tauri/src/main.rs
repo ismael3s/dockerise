6bc63d53d554bdfa24a3a631abe6fee9fcbbd424
Ismael3s <souz4ismael@gmail.com>
2024-02-04 21:42:09 -0300

    fix: deep search for csproj inside root folder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,8 @@
 mod dockerfile_builder;
 mod parse_dotnet_projects_reference;
 
-use std::{
-    fs::{self, DirEntry, File},
-    io::BufReader,
-    path::MAIN_SEPARATOR,
-};
+use std::{fs::File, io::BufReader, path::MAIN_SEPARATOR};
+use walkdir::WalkDir;
 
 use crate::{dockerfile_builder::DockerFilePath, parse_dotnet_projects_reference::Mermaid};
 
@@ -22,38 +19,23 @@ struct Output {
 }
 
 #[tauri::command]
-fn find_projects_files(path: String) -> Vec<String> {
-    let paths = fs::read_dir(path).unwrap();
-    let mut all_files: Vec<String> = Vec::new();
-    let project_folders = paths
-        .filter_map(Result::ok)
-        .filter(|entry| entry.path().is_dir())
-        .map(|entry| return entry.path());
-
-    for folder in project_folders {
-        let files = fs::read_dir(folder).unwrap();
-        let csproj_files = files
-            .filter_map(Result::ok)
-            .filter(|entry| entry.path().is_file())
-            .filter(|entry| filter_only_csproj(entry))
-            .map(|entry| return entry.path().to_str().unwrap().to_string());
-
-        all_files.extend(csproj_files);
-    }
-
-    return all_files;
-}
-
-fn filter_only_csproj(dir: &DirEntry) -> bool {
-    return matches!(dir.path().extension(), Some(extension) if extension == "csproj");
+fn find_projects_files(path: &str) -> Vec<String> {
+    return WalkDir::new(&path)
+        .into_iter()
+        .filter_entry(|e| !e.path().display().to_string().contains("test"))
+        .filter_map(|e| e.ok())
+        .filter(|file| matches!(file.path().extension(), Some(extension) if extension == "csproj"))
+        .map(|file| file.path().to_str().unwrap().to_string())
+        .collect::<_>();
 }
 
 #[tauri::command]
 fn my_custom_command(project_root: String, startup_project: String) -> Output {
     let mut projects: Vec<Project> = Vec::new();
     let mut docker_file_builder = dockerfile_builder::dotnet::new();
-    let startup_docker_file_path = DockerFilePath::new(&startup_project, MAIN_SEPARATOR).unwrap();
-    find_projects_files(project_root)
+    let startup_docker_file_path =
+        DockerFilePath::new(&startup_project, &project_root, MAIN_SEPARATOR).unwrap();
+    find_projects_files(&project_root)
         .iter()
         .for_each(|filename_with_path| {
             let file = BufReader::new(File::open(&filename_with_path).unwrap());
@@ -62,7 +44,7 @@ fn my_custom_command(project_root: String, startup_project: String) -> Output {
             project.update_project_name(&filename_with_path);
             projects.push(project);
             // TODO: need to handle err causes, because the dockerfile will be invalid when trying to build.
-            match DockerFilePath::new(&filename_with_path, MAIN_SEPARATOR) {
+            match DockerFilePath::new(&filename_with_path, &project_root, MAIN_SEPARATOR) {
                 Ok(docker_file_path) => {
                     docker_file_builder
                         .copy_csproj(&docker_file_path)
